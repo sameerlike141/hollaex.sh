3f9d36b462b7ff1fd3902fec66a85c5169afa79b
sameerlike141 <sameerlike141@gmail.com>
2022-02-02 22:21:28 +0500

    Create hollaex.sh

---
+++ hollaex.sh
@@ -0,0 +1,22 @@
+sudo dd if=/dev/zero bs=1M count=5021 of=/mnt/5GiB.swap
+sudo chmod 600 /mnt/5GiB.swap
+sudo mkswap /mnt/5GiB.swap
+sudo swapon /mnt/5GiB.swap
+
+git clone https://github.com/bitholla/hollaex-kit.git
+cd hollaex-kit
+bash install.sh
+
+hollaex server --setup
+
+hollaex server --start
+
+hollaex web --setup
+
+hollaex web --start
+
+hollaex prod
+
+hollaex web --build
+
+hollaex web --apply
